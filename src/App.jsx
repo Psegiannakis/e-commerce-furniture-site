0f9057a5bb452e0e6eddefc1537eda6a371c7f55
Teri P <psegiannakis@outlook.com>
2024-03-02 11:08:50 +1100

    carousel addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,28 @@
-export default function App() {}
+import Carousel from "./carousel";
+import "./card.css";
+
+export default function App() {
+  return (
+    <div className="cards-list">
+      <Carousel img="./couch.png" title="Couches" />
+
+      <Carousel img="./cabinet.png" title="Storage" />
+
+      <Carousel img="./bed.png" title="Bedroom" />
+
+      <Carousel img="./tables.png" title="Dining" />
+
+      <Carousel img="./lamp.png" title="Lighting" />
+
+      <Carousel img="./rug.png" title="Rugs & Carpets" />
+
+      <Carousel img="./decoration.png" title="Decoration" />
+
+      <Carousel img="./outdoor-chair.png" title="Outdoor" />
+
+      <Carousel img="./plant.png" title="Plants & Pots" />
+
+      <Carousel img="pan.png" title="Kitchenware" />
+    </div>
+  );
+}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./header.css";
 
 export default function Header() {
   return (
